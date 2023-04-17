f341a5bfbe4ad4fd884175acd68205dec91df182
sanjivsmoke <nirmalkumar22@hotmail.com>
2023-04-18 01:47:20 +0530

    bug_is_add

--- components/nav-bar/popover/darkmode_popover.js
+++ components/nav-bar/popover/darkmode_popover.js
@@ -1,5 +1,6 @@
-import { Popover, Button, Text, Link } from "@nextui-org/react";
+import { Popover, Button, Text, Link, Row } from "@nextui-org/react";
 import { AiOutlineBug } from "react-icons/ai";
+import TypewriterComponent from "typewriter-effect";
 
 const DarkModerPopover = () => {
   return (
@@ -7,16 +8,48 @@ const DarkModerPopover = () => {
       <Popover.Trigger>
         <Button color="gradient" auto icon={<AiOutlineBug size="20" />} />
       </Popover.Trigger>
-      <Popover.Content  css={{ alignItems: "center"}}>
-        <Text css={{ p: "$10", textAlign: "match-parent"}}>
-        Congratulations you found `the` bug which is in my code,its better to kill by hitting it.
+      <Popover.Content  css={{ alignItems: "center" }}>
+        <Text css={{ p: "$10",  textAlign: "match-parent"}}>
+        Congratulations on finding Bugs.
+        <TypewriterComponent
+            options={{
+              strings: ["Bugs are meant to be killed","But which one !?"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
         </Text>
+        <Row css={{p:"$2"}} justify="space-evenly">
         <Link
+        css={{gap:"$20" }}
           href="https://www.youtube.com/shorts/_zm1YI9wMx0"
           rel="noopener noreferrer"
           target="_blank">
           <AiOutlineBug size="20" />
         </Link>
+        <Link
+        css={{gap:"$20",color:"$cyan700" }}
+          href="https://www.youtube.com/shorts/pmAGRt1Z0i4"
+          rel="noopener noreferrer"
+          target="_blank">
+          <AiOutlineBug size="20" />
+        </Link>
+        <Link
+        css={{gap:"$20",color:"GoldenRod" }}
+          href="https://www.youtube.com/shorts/aftQAbHdXZs"
+          rel="noopener noreferrer"
+          target="_blank">
+          <AiOutlineBug size="20" />
+        </Link>
+        <Link
+        css={{gap:"$20" ,color:"$red800"}}
+          href="https://www.youtube.com/shorts/P9ZkTIgsByk"
+          rel="noopener noreferrer"
+          target="_blank">
+          <AiOutlineBug size="20" />
+        </Link>
+        </Row>
+        
       </Popover.Content>
     </Popover>
   );
